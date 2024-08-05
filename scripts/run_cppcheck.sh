dc91fccafd704344fe4262faadaaee48677f6fbc
Bogdanctx <Bogdanctx@users.noreply.github.com>
2024-08-05 12:24:12 +0300

    try again cppcheck

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -3,4 +3,9 @@
 cppcheck --enable=all \
     --inline-suppr \
     --project="${BUILD_DIR:-build}"/compile_commands.json \
+    -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
+    -i"${EXT_DIR:-ext}" --suppress="*:${EXT_DIR:-ext}/*" \
+    --suppress=missingIncludeSystem \
+    --suppress=unmatchedSuppression \
+    --suppress=useStlAlgorithm \
     --error-exitcode=1

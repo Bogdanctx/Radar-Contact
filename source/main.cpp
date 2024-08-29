54154d0248f64aaffa67aac123020f9c92a53a92
bogdan <trifan.bogdan09@yahoo.com>
2024-08-29 21:24:06 +0300

    -

--- source/main.cpp
+++ source/main.cpp
@@ -1,25 +1,37 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
-#include "ResourcesManager.hpp"
-#include "StateMachine.hpp"
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
-//////////////////////////////////////////////////////////////////////
-/// NOTE: this include is needed for environment-specific fixes     //
-/// You can remove this include and the call from main              //
-/// if you have tested on all environments, and it works without it //
-#include "env_fixes.h"                                              //
-//////////////////////////////////////////////////////////////////////
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-int main() {
-    ////////////////////////////////////////////////////////////////////////
-    /// NOTE: this function call is needed for environment-specific fixes //
-    init_threads();                                                       //
-    ////////////////////////////////////////////////////////////////////////
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            std::cout << "AA\n";
+        }
 
-    try {
-        ResourcesManager::Instance().load();
-        StateMachine::Instance().run();
-    } catch(std::runtime_error &err) {
-        std::cout << err.what();
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        // window.draw(...);
+
+        // end the current frame
+        window.display();
     }
+
+    return 0;
 }

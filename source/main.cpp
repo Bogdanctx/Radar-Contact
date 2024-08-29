541de960e4a7bcbe90ec6e345e104af9379a8b58
bogdan <trifan.bogdan09@yahoo.com>
2024-08-29 21:34:54 +0300

    -

--- source/main.cpp
+++ source/main.cpp
@@ -1,35 +1,22 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
 
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    // run the program as long as the window is open
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-        {
-            std::cout << "AA\n";
-        }
-
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-
-        // draw everything here...
-        // window.draw(...);
-
-        // end the current frame
+        window.clear();
+        window.draw(shape);
         window.display();
     }
 
